52eab11d921915f053c8739325b94c5a24ccb4f3
anirudhb <anirudhb@users.noreply.github.com>
2021-12-03 19:13:20 -0800

    day1: foldl -> foldl'

--- day1/part1.hs
+++ day1/part1.hs
@@ -6,7 +6,7 @@ windows :: Int -> [a] -> [[a]]
 windows n xs = transpose (take n (tails xs))
 
 numIncreases :: [Int] -> Int
-numIncreases xs = foldl go 0 $ windows 2 xs
+numIncreases xs = foldl' go 0 $ windows 2 xs
     where
         go x [a, b] = x + fromEnum (b > a)
         go x _ = x
--- day1/part2.hs
+++ day1/part2.hs
@@ -8,7 +8,7 @@ windows :: Int -> [a] -> [[a]]
 windows n xs = transpose (take n (tails xs))
 
 numIncreases :: [Int] -> Int
-numIncreases xs = foldl go 0 $ windows 2 $ windows 3 xs
+numIncreases xs = foldl' go 0 $ windows 2 $ windows 3 xs
     where
         go x [a, b] = x + fromEnum (sum b > sum a)
         go x _ = x
